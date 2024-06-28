5a1e75671521caf74a08f5923f07bddc04688b53
Almog Khaikin <almogkh@gmail.com>
2024-06-28 19:37:05 +0300

    Add support for running local DB

--- src/lib/db/db.server.ts
+++ src/lib/db/db.server.ts
@@ -1,9 +1,14 @@
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
+import { drizzle as drizzleNode } from "drizzle-orm/node-postgres";
 import { sql as vercelSql } from "@vercel/postgres";
+import Pg from "pg";
 import * as schema from "./schema";
 import { and, eq, lt, sql } from "drizzle-orm";
 
-export const db = drizzle(vercelSql, {schema});
+const { Pool } = Pg;
+export const db = process.env.NODE_DB
+                    ? drizzleNode(new Pool({connectionString: process.env.POSTGRES_URL}))
+                    : drizzleVercel(vercelSql, {schema});
 
 export async function getUserSession(sessionid: string) {
     const session = db.select().from(schema.sessions).where(and(eq(schema.sessions.sessionid, sessionid), lt(sql`now()`, schema.sessions.expirationTime))).as('session');
