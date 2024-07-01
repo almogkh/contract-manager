f91366d4cafc6b8e4573b8ed8b87ff03e1f993aa
Almog Khaikin <almogkh@gmail.com>
2024-07-01 18:25:22 +0300

    Make all the buttons have equal width

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col items-center space-y-4">
     {#each data.actions as action}
-        <a href={action.route} class="border p-2 bg-slate-400">
+        <a href={action.route} class="border p-2 bg-slate-400 w-full text-center">
             {action.name}
         </a>
     {/each}
