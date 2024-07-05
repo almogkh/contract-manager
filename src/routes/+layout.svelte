92fa747b3f6433ee3313df61312eadf906f175b1
Almog Khaikin <almogkh@gmail.com>
2024-07-05 19:35:15 +0300

    Adjust top-level layout slightly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,14 +5,14 @@ import "../app.css";
     export let data;
 </script>
 
-<div class="bg-slate-300 h-screen flex flex-col items-center">
+<div class="bg-slate-300 min-h-screen flex flex-col items-center">
     <header class="flex justify-end w-full p-2 border-b mb-4">
         <form method="post" action="/login?/logout" use:enhance>
             <button class="{data.user ? "" : "invisible"} p-1 mr-2 border rounded-md bg-red-600 text-white">Logout</button>
         </form>
     </header>
 
-    <main class="flex flex-col items-center">
+    <main class="flex flex-col items-center px-2">
         <slot />
     </main>
 
