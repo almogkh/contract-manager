1842e65c61ac5b9597dffd14e99da0a2365bff24
Nir-betesh <Nirb1199@gmail.com>
2024-07-18 19:30:41 +0300

    Added some mobile adjustments

--- src/routes/contractStatus/+page.svelte
+++ src/routes/contractStatus/+page.svelte
@@ -2,17 +2,13 @@
     import { enhance } from "$app/forms";
 
     export let data;
-
-    const handleComplete = () => {
-        
-    }
 </script>
 
 <div class="grid grid-cols-4 gap-4 p-8">
     <span class="font-bold text-lg border-b-2 border-black pb-2 col-span-1">ID</span>
     <span class="font-bold text-lg border-b-2 border-black pb-2 col-span-1">Floor</span>
     <span class="font-bold text-lg border-b-2 border-black pb-2 col-span-1">Number</span>
-    <span class="font-bold text-lg border-b-2 border-black pb-2 col-span-1">Update Status</span>
+    <span class="font-bold text-lg border-b-2 border-black pb-2 col-span-1">Status</span>
     
     {#each data.apartmentsList as apt}
         <div class="col-span-4 grid grid-cols-4 gap-4 border-b-4 border-gray-500 py-2">
@@ -25,7 +21,7 @@
                 <input type="hidden" name="floor" value={apt.floor}>
                 <input type="hidden" name="number" value={apt.number}>
                 <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-1.5 px-2 rounded focus:outline-none focus:shadow-outline">
-                    Complete
+                    Done
                 </button>
             </form>
         </div>
