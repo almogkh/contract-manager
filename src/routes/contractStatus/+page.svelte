6e56ab54b7fbec63d6469bc9a148d70944088394
Almog Khaikin <almogkh@gmail.com>
2024-08-18 18:01:42 +0300

    Make contract status change unidirectional

--- src/routes/contractStatus/+page.svelte
+++ src/routes/contractStatus/+page.svelte
@@ -23,47 +23,32 @@
 
 </script>
 
-<div class="text-2xl grid grid-cols-5 gap-4 p-8">
+<div class="text-2xl grid grid-cols-4 gap-4 p-8">
     <span class="border-b-2 border-black pb-2 col-span-1 font-bold">Contract ID</span>
-    <span class="border-b-2 border-black pb-2 col-span-1 font-bold">Current</span>
-    <span class="border-b-2 border-black pb-2 col-span-1 font-bold">New Status</span>
+    <span class="border-b-2 border-black pb-2 col-span-1 font-bold">Current Status</span>
     <span class="pb-2 col-span-1 font-bold"></span>
     <span class="pb-2 col-span-1 font-bold"></span>
 </div>
 
+<div class="text-xl grid grid-cols-3 gap-4">
 {#each sortedContracts as contract, index}
-<form method="post" action="?/updateContractStatus" use:enhance={({ action }) => {
-    return ({ update }) => {
-        update({ reset: false });
-        toggleEdit(index);
-    };
-}} class="text-xl grid grid-cols-5 gap-4 border-b-4 border-gray-500 py-2">
-        <div class="col-span-5 grid grid-cols-5 gap-4 border-gray-500 py-2">
-            <span>{contract.id}</span>
-            <span>{contract.status === 'new' ? 'New' : 'In Progress'}</span>
+<form method="post" action="?/updateContractStatus" use:enhance class="contents">
+    <div class="col-span-3 grid grid-cols-3 gap-4 border-gray-500 py-2">
+        <span>{contract.id}</span>
+        <span>{contract.status === 'new' ? 'New' : 'In Progress'}</span>
 
-            <select name="status" bind:value={status[index]} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" disabled={!toggleBtn[index]}>
-                <option value="new">New</option>
-                <option value="inprogress">In Progress</option>
-                <option value="complete">Complete</option>
-            </select>
-
-            <input type="hidden" name="id" value={contract.id}>
-            <input type="hidden" name="dueDate" value={contract.dueDate}>
-
-            {#if toggleBtn[index]}
-            <button type="submit" class="bg-green-500 hover:bg-green-700 text-white py-1.5 px-2 rounded focus:outline-none focus:shadow-outline">
-                Save
-            </button>
-            <button type="button" on:click={() => toggleEdit(index)} class="bg-red-500 hover:bg-red-700 text-white py-1.5 px-2 rounded focus:outline-none focus:shadow-outline">
-                Cancel
-            </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white py-1.5 px-2 rounded focus:outline-none focus:shadow-outline">
+            {#if contract.status === 'new'}
+                Mark as In Progress
             {:else}
-            <button type="button" on:click={() => toggleEdit(index)} class="bg-blue-500 hover:bg-blue-700 text-white py-1.5 px-2 rounded focus:outline-none focus:shadow-outline">
-                Edit
-            </button>
+                Mark as Complete
             {/if}
-        </div>
-    </form>
+        </button>
+        <input type="hidden" name="status" value={contract.status === 'new' ? 'inprogress' : 'complete'}/>
+        <input type="hidden" name="id" value={contract.id}/>
+        <input type="hidden" name="dueDate" value={contract.dueDate}/>
+    </div>
+</form>
+<span class="col-span-3 border-b-4 border-gray-500"/>
 {/each}
-
+</div>
